95670ac8a364a785ca34a9abaa555021246b81fa
Thalys001 <thalyssouza708@gmail.com>
2023-10-15 16:23:10 -0300

    feat: creating function to remove tasks

--- src/components/Tasks/styles.ts
+++ src/components/Tasks/styles.ts
@@ -2,21 +2,21 @@ import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
   container: {
-    height: 'auto',
-    width: '100%',
+    height: 60,
+    width: 300,
     backgroundColor: '#262626',
     borderRadius: 8,
     borderColor: '#333',
     // boxShadow: '0 2 8 0 rgba(0, 0, 0, 0.06)',
     flexDirection: 'row',
     alignItems: 'center',
+    justifyContent: 'center',
     marginBottom: 8,
     paddingVertical: 12,
     paddingHorizontal: 12,
   },
   text: {
     flex: 1,
-    height: 40,
     fontSize: 16,
     color: '#F2F2F2',
     marginHorizontal: 8
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { View, TextInput, TouchableOpacity, FlatList } from "react-native";
+import { View, TextInput, TouchableOpacity, FlatList, Alert, Text } from "react-native";
 
 import { styles } from "./styles";
 import ToDo from "../../../assets/icons/todo";
@@ -11,11 +11,26 @@ export function Home() {
   const [tasksName, setTasksName] = useState('');
 
   function handleTasksAdd() {
-    console.log("adicionar");
-    setTasks(prevState => [...prevState, tasksName]);
-    setTasksName('')
+    tasksName ? setTasks(prevState => [...prevState, tasksName]) : null;
+    setTasksName('');
   }
-  
+
+  function handleTasksRemove(text: string) {
+    Alert.alert("Remover", `Remover a tarefa: ${text}?`, [
+      {
+        text: 'Sim',
+        onPress: () => {
+          setTasks(prevState => prevState.filter(tasks => tasks !== text)),
+            Alert.alert("Deletar", `a tarefa: ${text} foi deletada!`)
+        }
+      },
+      {
+        text: 'Não',
+        style: 'cancel'
+      }
+    ])
+  }
+
   return (
     <View style={styles.container}>
       <View style={styles.header}>
@@ -42,7 +57,8 @@ export function Home() {
           <Tasks
             key={item}
             text={item}
-             />
+            onRemove={() => handleTasksRemove(item)}
+          />
         )}
       />
     </View>
